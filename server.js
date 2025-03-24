b6862de84d69f4bf4f04ea55e9af03538871fa7b
ChatSites™ <rjewett459@gmail.com>
2025-03-23 17:48:14 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
 
+// Send index.html when someone visits "/"
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Use the port provided by Render or default to 3000 locally
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
